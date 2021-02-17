f111aa8686813ef8ff147c159558def806233755
Anu <anamika_sawhney@rediffmail.com>
2021-02-17 16:06:56 +0530

    Added Project

---
+++ FinalCheck/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.Cognizant.Truyum.Model
+{
+    public class MenuItem 
+    {
+        
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        string name;
+
+        float price;
+        bool active;
+        DateTime dateOfLaunch;
+        string category;
+        bool freeDelivery;
+        public MenuItem() { }
+        public MenuItem(long id,string name,float price,
+        bool active,
+        DateTime dateOfLaunch,
+        string category,
+        bool freeDelivery)
+        {
+            this.id = id;
+            this.name = name;
+            this.price = price;
+            this.active = active;
+            this.dateOfLaunch = dateOfLaunch;
+            this.category = category;
+            this.freeDelivery = freeDelivery;
+
+        }
+    }
+    public class Cart
+    {
+        List<MenuItem> menuItemList;
+        double total;
+    }
+}
